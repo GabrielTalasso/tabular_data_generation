48ceff40d4cd35c970c3bd9f35dfea750b251dd3
GabrielTalasso <gukstin@gmail.com>
2023-10-20 11:31:47 -0300

    modified:   ML_efficiency.py 	modified:   __pycache__/ML_efficiency.cpython-38.pyc 	modified:   evaluate_generations.ipynb

--- ML_efficiency.py
+++ ML_efficiency.py
@@ -4,6 +4,8 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
 from sklearn.neural_network import MLPClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+import pandas as pd
 
 
 class MLE():
@@ -17,7 +19,19 @@ class MLE():
         self.real_dataframe = real_dataframe
         self.target_column = target_column
 
-    def eval(self, metric):
+    def preprocessing(self):
+
+        cols = self.synthetic_dataframe.columns
+        num_cols = self.synthetic_dataframe._get_numeric_data().columns
+        cat_cols = list(set(cols) - set(num_cols))
+        self.synthetic_dataframe = pd.get_dummies(self.synthetic_dataframe, columns=cat_cols)
+
+        cols = self.real_dataframe.columns
+        num_cols = self.real_dataframe._get_numeric_data().columns
+        cat_cols = list(set(cols) - set(num_cols))
+        self.real_dataframe = pd.get_dummies(self.real_dataframe, columns=cat_cols)
+
+    def eval(self):
 
         train = self.synthetic_dataframe.drop(self.target_column, axis = 1)
         train_target = self.synthetic_dataframe[self.target_column]
@@ -38,6 +52,7 @@ class MLE():
 
 
         MLPC = MLPClassifier(hidden_layer_sizes=[10,10], random_state=1)
+        MLPC.fit(train, train_target)
         mlpc_pred = MLPC.predict(test)
         acc_mlpc = accuracy_score(mlpc_pred, test_target)
 
@@ -46,3 +61,39 @@ class MLE():
         DTC.fit(train, train_target)
         dtc_pred = DTC.predict(test)
         acc_dtc = accuracy_score(dtc_pred, test_target)
+
+        report_synthetic = [acc_abc, acc_lrc, acc_mlpc,  acc_dtc]
+
+        ## original acc
+
+        X = self.real_dataframe.drop(self.target_column, axis = 1)
+        y = self.real_dataframe[self.target_column]
+
+        train,  test, train_target, test_target = train_test_split(X, y, train_size=0.8, random_state=1)
+
+        ABC = AdaBoostClassifier(random_state=1)
+        ABC.fit(train, train_target)
+        abc_pred = ABC.predict(test)
+        acc_abc = accuracy_score(abc_pred, test_target)
+
+
+        LRC = LogisticRegression(random_state=1)
+        LRC.fit(train, train_target)
+        lrc_pred = LRC.predict(test)
+        acc_lrc = accuracy_score(lrc_pred, test_target)
+
+
+        MLPC = MLPClassifier(hidden_layer_sizes=[10,10], random_state=1)
+        MLPC.fit(train, train_target)
+        mlpc_pred = MLPC.predict(test)
+        acc_mlpc = accuracy_score(mlpc_pred, test_target)
+
+
+        DTC = DecisionTreeClassifier(random_state=1)
+        DTC.fit(train, train_target)
+        dtc_pred = DTC.predict(test)
+        acc_dtc = accuracy_score(dtc_pred, test_target)
+
+        report_original = [acc_abc, acc_lrc, acc_mlpc,  acc_dtc]
+
+        return report_synthetic, report_original
